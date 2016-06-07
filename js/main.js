5e09966a5dbdd1a95292c89c13744d033904ada9
Wayne Graham <wgraham@clir.org>
2016-06-07 13:10:30 -0400

    Add image slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,49 @@
-/* global $, console */
+/* global jQuery, console, document, QueryLoader2, window */
 "use strict";
 
-function footer(){
-  var clir = '';
-  console.log(clir);
-}
+jQuery(function($) {
+    // Superslide slider
+    $('#slides').superslides({
+        animation: 'fade',
+        animation_easing: 'linear',
+        pagination: 'true',
+        animation_speed: 1000
+    });
 
-footer();
+    // Scroll Up
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
+
+// Preloader
+window.addEventListener('DOMContentLoaded', function() {
+    new QueryLoader2(document.querySelector('body'), {
+        barColor: "#efefef",
+        backgroundColor: "#111",
+        percentage: true,
+        barHeight: 1,
+        minimumTime: 200,
+        fadeOutTime: 1000
+    });
+
+    function footer() {
+        var clir = '';
+        // console.log(clir);
+    }
+
+    footer();
+});
