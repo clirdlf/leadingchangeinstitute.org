cc80e602dbf6cff8bc9a93e332a088ccab875d5b
Wayne Graham <wgraham@clir.org>
2016-06-28 15:18:44 -0400

    Fix for gulp-sass and browserReload

--- gulpfile.js
+++ gulpfile.js
@@ -40,15 +40,16 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
+    return gulp.src('_sass/main.scss')
         .pipe(sass({
-            includePaths: ['scss'],
+            // includePaths: ['scss'],
             onError: browserSync.notify
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(browserSync.reload({stream:true}));
+        // .pipe(gulp.dest('css'));
 });
 
 /**
@@ -56,8 +57,8 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/**/*.scss', ['sass']);
-    gulp.watch(['*.html', '**/*.md', '_scss/*.scss', '_layouts/*.html', '_posts/*', '_institutes/*'], ['jekyll-rebuild']);
+    gulp.watch('_sass/*.scss', ['sass']);
+    gulp.watch(['*.html', '**/*.md', '_layouts/*.html', '_posts/*', '_institutes/*'], ['jekyll-rebuild']);
 });
 
 /**
